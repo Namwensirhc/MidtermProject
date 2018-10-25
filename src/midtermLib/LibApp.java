7978079d303e3705412e4cf491b9c3bda4af6ef8
chris <new826@gmail.com>
2018-10-25 15:02:41 -0400

    notes in classes

--- src/midtermLib/LibApp.java
+++ src/midtermLib/LibApp.java
@@ -4,7 +4,12 @@ public class LibApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//Ask user for search/ Bring up list
+		// Display book list
+		// If/Else for book status:
+//		If book available assign due date
+//		else is out,
+//		Have some books checked out with due dates for coming back
 	}
 
 }
--- src/midtermLib/LibTexts.java
+++ src/midtermLib/LibTexts.java
@@ -1,5 +1,8 @@
 package midtermLib;
 
 public class LibTexts {
-
+	// HARDCODE THESE?????
+//Book list, author title ketwords
+	// bookstatuses
+	// Books in Lib (Available and out)
 }
