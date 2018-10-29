7097693967b94ab5dc68d967fa454f02ac9de81e
Dejuan Johnson <dejuanj86@gmail.com>
2018-10-29 09:29:52 -0400

    Update EditFileList.java

--- src/midtermLib/EditFileList.java
+++ src/midtermLib/EditFileList.java
@@ -1,59 +1,176 @@
 package midtermLib;
-
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.PrintWriter;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Scanner;
 
-public class EditFileList {
+public class EditTextLine {
+
+public static void changeTextLine(String fileName, String newLine, int lineNumber) {
+        String content = new String();
+        String editedContent = new String();
+        String books = readFromFile(fileName);
+        editedContent = editLineInContent(content, newLine, lineNumber);
+        writeToFile(fileName, editedContent);
+
+    }
+
+private static String readFromFile(String fileName) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+private static int numberOfLinesInFile(String content) {
+    int numberOfLines = 0;
+    int index = 0;
+    int lastIndex = 0;
+
+    lastIndex = content.length() - 1;
+
+    while (true) {
+
+        if (content.charAt(index) == '\n') {
+            numberOfLines++;
+
+        }
+
+        if (index == lastIndex) {
+            numberOfLines = numberOfLines + 1;
+            break;
+        }
+        index++;
+
+    }
+
+    return numberOfLines;
+}
+
+private static String[] turnFileIntoArrayOfStrings(String content, int lines) {
+    String[] array = new String[lines];
+    int index = 0;
+    int tempInt = 0;
+    int startIndext = 0;
+    int lastIndex = content.length() - 1;
+
+    while (true) {
+
+        if (content.charAt(index) == '\n') {
+            tempInt++;
+
+            String temp2 = new String();
+            for (int i = 0; i < index - startIndext; i++) {
+                temp2 += content.charAt(startIndext + i);
+            }
+            startIndext = index;
+            array[tempInt - 1] = temp2;
+
+        }
+
+        if (index == lastIndex) {
+
+            tempInt++;
+
+            String temp2 = new String();
+            for (int i = 0; i < index - startIndext + 1; i++) {
+                temp2 += content.charAt(startIndext + i);
+            }
+            array[tempInt - 1] = temp2;
+
+            break;
+        }
+        index++;
+
+    }
+
+    return array;
+}
+
+private static String editLineInContent(String content, String newLine, int line) {
+
+    int lineNumber = 0;
+    lineNumber = numberOfLinesInFile(content);
+
+    String[] lines = new String[lineNumber];
+    lines = turnFileIntoArrayOfStrings(content, lineNumber);
+
+    if (line != 1) {
+        lines[line - 1] = "\n" + newLine;
+    } else {
+        lines[line - 1] = newLine;
+    }
+    content = new String();
+
+    for (int i = 0; i < lineNumber; i++) {
+        content += lines[i];
+    }
+
+    return content;
+}
+
+private static void writeToFile(String file, String books) {
+
+    try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "utf-8"))) {
+        writer.write(books);
+    } catch (UnsupportedEncodingException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    } catch (FileNotFoundException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+}
+
+private static String readFromFile(String fileName, String books) {
+    String books1 = null;
+    File file = new File(fileName);
+    FileReader reader = null;
+    try {
+        reader = new FileReader(file);
+        char[] chars = new char[(int) file.length()];
+        reader.read(chars);
+        books1 = new String(chars);
+        reader.close();
+    } catch (IOException e) {
+        e.printStackTrace();
+    } finally {
+        if (reader != null) {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                
+                e.printStackTrace();
+            }
+        }
+    }
+    return books1;
+}
+
+static void createFile(String fileName) {
 	
-	public static String checkOut(ArrayList<?> lines, int input) {
-		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		Date date = new Date();
-		String today = (dateFormat.format(date));
-		LocalDate datePlus = LocalDate.now().plusDays(14);
-		String dueDate = datePlus.toString();
-
-		String temp = (String) lines.get(input - 1);
-		if (temp.contains("In Stock")) {
-			temp = temp.replaceAll("\\bIn Stock\\b", " Checked Out: " + today);
-			temp = temp + " Due Back: " + dueDate;
-		}
-		return temp;
-	}
 	
-	public static ArrayList<String> getListFromFile(File f) throws FileNotFoundException {
-		Scanner s;
-		ArrayList<String> list = new ArrayList<String>();
-		s = new Scanner(f);
-		while (s.hasNextLine()) {
-			list.add(s.nextLine());
-		}
-		s.close();
-		return list;
-	}
+}
 
-	public static void writeToFile(String directoryFolder, String fileName, ArrayList<String> input) {
-		Path filePath = Paths.get(directoryFolder, fileName);
-		File file = filePath.toFile();
-		
-		ArrayList<String> lines = input;
-
-		try {
-			PrintWriter outW = new PrintWriter(new FileOutputStream(file, true));
-			outW.println(lines);
-		} catch (FileNotFoundException e) {
-			System.out.println("Error - 404: File not found....HaHa!");
-		}
-
-	}
+static void readFromFile(String fileName, ArrayList<Book> bookList) {
 	
+	
+}
+
+static void writeToFile(ArrayList<Book> bookList, String fileName) {
+	
+	
+}
+
+
+
+
+
 }
